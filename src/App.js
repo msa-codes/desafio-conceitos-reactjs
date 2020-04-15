c0420ad4a79dff2a376e2a28fc1ef8f22629561b
Marcelo da Silva Ataíde <contato.marceloataide@gmail.com>
2020-04-15 08:29:17 -0300

    Add and remove Repository

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    try {
+      const response = await api.post('repositories', {
+        title: `Novo repositório ${Date.now()}`,
+        url: "https://github.com/msa-codes/desafio-conceitos-node",
+        techs: ['NodeJS', 'ReactJS']
+      });
+  
+      const repository = response.data;
+  
+      setRepositories([...repositories, repository]);
+    } catch(err) {
+      alert('Erro ao adicionar repositório, tente novamente.');
+    }
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    try {
+      await api.delete(`repositories/${id}`);
+
+      setRepositories(repositories.filter(repository => repository.id !== id));
+    } catch(err) {
+      alert('Erro ao deletar repositório, tente novamente.');
+    }
   }
 
   return (
-    <div>
+    <div className="container">
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map(repository => (
+          <li key={repository.id}>
+            <span>
+              {repository.title}
+            </span>
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
